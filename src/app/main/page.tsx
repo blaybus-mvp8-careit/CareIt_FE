a55f9e1bcd72395ad56650356df109b4c9c67185
EJ <sbjdnm@gmail.com>
2025-02-17 05:38:48 +0900

    feat: 메인페이지 전체 구조와 이미지 추가 작업

---
+++ src/app/main/page.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import Footer from '@/components/Footer/Footer'
+import Header from '@/components/Header/Header'
+import BannerSwiper from './components/BannerSwiper'
+import MatchingRequestCarousel from './components/MatchingRequestSwiper'
+import WorkCondition from './components/WorkCondition'
+
+function MainPage() {
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 1000) // 1초 후 로딩 완료
+  }, [])
+
+  if (isLoading) {
+    return (
+      <div className='flex h-screen items-center justify-center text-lg font-bold text-gray-600'>
+        로딩 중...
+      </div>
+    )
+  }
+
+  return (
+    <div className='mb-24 w-full'>
+      <Header />
+      <WorkCondition />
+      <BannerSwiper />
+      <MatchingRequestCarousel />
+      <Footer />
+    </div>
+  )
+}
+
+export default MainPage
